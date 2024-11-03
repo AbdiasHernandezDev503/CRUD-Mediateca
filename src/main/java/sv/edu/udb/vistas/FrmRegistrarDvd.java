8720f96d7907a3947ac2c1a0217f33e52f9f25a4
Buffalo9009 <carlosmauriciomedranoortega@gmail.com>
2024-11-02 21:04:09 -0600

    poniendo la funcionalidad de insertar y modificar

--- src/main/java/sv/edu/udb/vistas/FrmRegistrarDvd.java
+++ src/main/java/sv/edu/udb/vistas/FrmRegistrarDvd.java
@@ -4,6 +4,12 @@
  */
 package sv.edu.udb.vistas;
 
+import javax.swing.JOptionPane;
+import sv.edu.udb.dao.DvdDAO;
+import sv.edu.udb.entidades.Dvd;
+
+
+
 /**
  *
  * @author Buffalo9009
@@ -16,6 +22,39 @@ public class FrmRegistrarDvd extends javax.swing.JFrame {
     public FrmRegistrarDvd() {
         initComponents();
     }
+    
+
+    /**
+     * ......................................................................................................................................................
+     */
+    public boolean validarDatos() {
+        if (txtTitulo.getText().trim().isEmpty()) {
+            JOptionPane.showMessageDialog(this, "El campo Título no puede estar vacío.", "Error", JOptionPane.ERROR_MESSAGE);
+            txtTitulo.requestFocus();
+            return false;
+        }
+        if (txtDirector.getText().trim().isEmpty()) {
+            JOptionPane.showMessageDialog(this, "El campo Director no puede estar vacío.", "Error", JOptionPane.ERROR_MESSAGE);
+            txtDirector.requestFocus();
+            return false;
+        }
+        if (txtDuracion.getText().trim().isEmpty()) {
+            JOptionPane.showMessageDialog(this, "El campo Duracion no puede estar vacío.", "Error", JOptionPane.ERROR_MESSAGE);
+            txtDuracion.requestFocus();
+            return false;
+        }
+        if (txtGenero.getText().trim().isEmpty()) {
+            JOptionPane.showMessageDialog(this, "El campo Genero no puede estar vacío.", "Error", JOptionPane.ERROR_MESSAGE);
+            txtGenero.requestFocus();
+            return false;
+        }
+        if ((int) txtStock.getValue() <= 0) {
+            JOptionPane.showMessageDialog(this, "El campo Stock no puede estar vacío.", "Error", JOptionPane.ERROR_MESSAGE);
+            txtStock.requestFocus();
+            return false;
+        }
+        return true;
+    }
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -26,21 +65,199 @@ public class FrmRegistrarDvd extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        txtDirector = new javax.swing.JTextField();
+        txtDuracion = new javax.swing.JTextField();
+        txtGenero = new javax.swing.JTextField();
+        txtStock = new javax.swing.JSpinner();
+        jLabel6 = new javax.swing.JLabel();
+        txtTitulo = new javax.swing.JTextField();
+        btnCancelar = new javax.swing.JButton();
+        btnGuardar = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        jLabel1.setText("Registrar DVD");
+
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        jLabel2.setText("Director:");
+
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        jLabel3.setText("Duracion:");
+
+        jLabel4.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        jLabel4.setText("Genero:");
+
+        jLabel5.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        jLabel5.setText("Stock:");
+
+        jLabel6.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        jLabel6.setText("Titulo:");
+
+        btnCancelar.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        btnCancelar.setText("cancelar");
+        btnCancelar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCancelarActionPerformed(evt);
+            }
+        });
+
+        btnGuardar.setText("Guardar");
+        btnGuardar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnGuardarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(62, 62, 62)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel6)
+                            .addComponent(jLabel1)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(59, 59, 59)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel3)
+                                    .addComponent(jLabel4)
+                                    .addComponent(jLabel5)
+                                    .addComponent(jLabel2))
+                                .addGap(33, 33, 33)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                        .addComponent(txtGenero, javax.swing.GroupLayout.DEFAULT_SIZE, 199, Short.MAX_VALUE)
+                                        .addComponent(txtDuracion)
+                                        .addComponent(txtStock, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                    .addComponent(txtDirector, javax.swing.GroupLayout.PREFERRED_SIZE, 199, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(txtTitulo, javax.swing.GroupLayout.PREFERRED_SIZE, 199, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(btnGuardar, javax.swing.GroupLayout.PREFERRED_SIZE, 79, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(btnCancelar, javax.swing.GroupLayout.PREFERRED_SIZE, 79, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                .addContainerGap(71, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(16, 16, 16)
+                .addComponent(jLabel1)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel6)
+                    .addComponent(txtTitulo, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(26, 26, 26)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel2)
+                    .addComponent(txtDirector, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(21, 21, 21)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel3)
+                    .addComponent(txtDuracion, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(25, 25, 25)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel4)
+                    .addComponent(txtGenero, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel5)
+                    .addComponent(txtStock, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(53, 53, 53)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnCancelar)
+                    .addComponent(btnGuardar, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(0, 9, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnCancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCancelarActionPerformed
+        dispose();
+    }//GEN-LAST:event_btnCancelarActionPerformed
+
+    private void btnGuardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGuardarActionPerformed
+        if (!validarDatos()) {
+            return;
+        }
+        if (btnGuardar.getText().equals("Guardar")) {
+
+            Dvd dvd = new Dvd();
+
+            dvd.setTitulo(this.txtTitulo.getText());
+            dvd.setDirector(this.txtDirector.getText());
+            /*dvd.setDuracion(this.txtDuracion.getText());**/
+            dvd.setGenero(this.txtGenero.getText());
+            dvd.setStock((int) this.txtStock.getValue());
+
+            DvdDAO dvdDAO = new DvdDAO();
+
+            try {
+                int result = dvdDAO.crear(dvd);
+
+                if (result > 0) {
+                    JOptionPane.showMessageDialog(null,
+                            "Los datos se han ingresado correctamente!!",
+                            "Éxito",
+                            JOptionPane.INFORMATION_MESSAGE);
+                  //  this.listarLibros.cargarLibros();
+
+                    dispose();
+                } else {
+                    JOptionPane.showMessageDialog(null,
+                            "Ocurrió un error inesperado al guardar los datos",
+                            "Error",
+                            JOptionPane.ERROR_MESSAGE);
+                }
+            } catch (Exception ex) {
+              //  log.error("Ocurrió el siguiente error: ", ex.getMessage());
+            }
+        } else if (btnGuardar.getText().equals("Modificar")) {
+            Dvd dvd = new Dvd();
+
+            dvd.setTitulo(this.txtTitulo.getText());
+            dvd.setDirector(this.txtDirector.getText());
+           // dvd.setDuracion(this.txtDuracion.getText());
+            dvd.setGenero(this.txtGenero.getText());
+            dvd.setStock((int) this.txtStock.getValue());
+
+            DvdDAO dvdDAO = new DvdDAO();
+
+            try {
+                int result = 0;
+               // int result = DvdDAO.modificar(dvd);
+
+                if (result > 0) {
+                    JOptionPane.showMessageDialog(null,
+                            "Los datos se han modificado correctamente!!",
+                            "Éxito",
+                            JOptionPane.INFORMATION_MESSAGE);
+                  //  this.listarDvd.cargarDvd();
+
+                    dispose();
+                } else {
+                    JOptionPane.showMessageDialog(null,
+                            "Ocurrió un error inesperado al modificar los datos",
+                            "Error",
+                            JOptionPane.ERROR_MESSAGE);
+                }
+            } catch (Exception ex) {
+               // log.error("Ocurrió el siguiente error: ", ex.getMessage());
+                JOptionPane.showMessageDialog(this, "Ocurrió un error al modificar los datos.", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }//GEN-LAST:event_btnGuardarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +295,18 @@ public class FrmRegistrarDvd extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnCancelar;
+    private javax.swing.JButton btnGuardar;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JTextField txtDirector;
+    private javax.swing.JTextField txtDuracion;
+    private javax.swing.JTextField txtGenero;
+    private javax.swing.JSpinner txtStock;
+    private javax.swing.JTextField txtTitulo;
     // End of variables declaration//GEN-END:variables
 }
